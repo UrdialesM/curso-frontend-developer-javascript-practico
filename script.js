8c7baed8c88216c1005cf3fff0214173f88193af
Manuel Urdiales <manuelurdiales7@gmail.com>
2023-03-16 15:19:19 -0300

    fusion-menu-mobile

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const menuBurger = document.querySelector('.burger-menu');
+
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuBurger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
