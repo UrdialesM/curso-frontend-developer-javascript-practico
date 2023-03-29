265bfb4f1a487435d24cf9307217d2aa37c11c8c
Manuel Urdiales <manuelurdiales7@gmail.com>
2023-03-29 13:11:23 -0300

    maquetacion-js-dom

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const menuMobile = document.querySelector('.mobile-menu');
 const menuBurger = document.querySelector('.burger-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+const mainContainer = document.querySelector('.main-container');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleMobileMenu);
@@ -24,3 +26,59 @@ function toggleAsideMenu () {
     menuMobile.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 }
+ 
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: 320,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: 'pantalla',
+    price: 509,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: 'celular',
+    price: 230,
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+function renderProducts(arr){
+for(product of arr){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.img);
+
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    
+
+    const productInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    productInfoDiv.append(productPrice, productName);
+    
+    
+    const figure = document.createElement('figure');
+    const imgFigure = document.createElement('img');
+    imgFigure.setAttribute('src', "./icons/bt_add_to_cart.svg");
+
+    figure.appendChild(imgFigure);
+    
+    productInfo.append(productInfoDiv, figure);
+    productCard.append(img, productInfo);
+    
+    cardsContainer.appendChild(productCard);
+    mainContainer.appendChild(cardsContainer);
+    
+
+    
+}
+}
+renderProducts(productList);
